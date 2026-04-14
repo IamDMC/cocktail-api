d19c44019f9d425aa9a58eb6929f03f51d5e5409
IamDMC <daniel.czosz@gmx.com>
2026-04-14 11:36:54 +0200

    refactor(category): introduce CategoryUpdateRequest

---
+++ app/Http/Requests/Category/CategoryUpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use App\Models\Category;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+/**
+ * @property  Category $category
+ */
+class CategoryUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'min:4',
+                'max:60',
+               Rule::unique('categories')->ignore($this->category)
+            ],
+            'description' => ['required', 'string', 'min:4', 'max:255']
+        ];
+    }
+}
